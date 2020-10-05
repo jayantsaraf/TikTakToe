e8c71718b0be332a14e6e969a7c291c09ffe5827
jayantsaraf <sarafjayant24@gmail.com>
2020-10-05 17:30:20 +0530

    [jayant]Add. added a function for user to move to a new position

--- Tik_Tak_Toe/Program.cs
+++ Tik_Tak_Toe/Program.cs
@@ -3,13 +3,17 @@
 namespace TikTakToe
 {
     public class Program
-    {      
+    {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Tik Tak Toe program!");
             TikTakToeGame game = new TikTakToeGame();
             game.CreateBoard();
             game.Input();
+            game.ShowBoard();
+            Console.WriteLine("Enter the position starting from top left you want to enter your next character");
+            int input = int.Parse(Console.ReadLine());
+            game.Move(input);
         }
     }
 }
---
+++ Tik_Tak_Toe/TikTakToeGame.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TikTakToe
+{
+    public class TikTakToeGame
+    {
+        char[] board = new char[10];
+        public void CreateBoard()
+        {
+            int i = 0;
+            for (i = 1; i < 10; i++)
+            {
+                board[i] = ' ';
+            }
+        }
+        public void Input()
+        {
+            int i = 1;
+            Console.WriteLine("What do you want to input(X or O)?");
+            char input = char.Parse(Console.ReadLine());
+            board[i] = input;
+            i++;
+        }
+        public void ShowBoard()
+        {
+            int c = 1;
+            for (int i = 1; i <= 3; i++)
+            {
+                for (int j = 1; j <= 3; j++)
+                {
+                    Console.Write(board[c] + " | ");
+                    c++;
+                }
+                Console.WriteLine("\n");
+                Console.WriteLine("-------------------");  
+            }
+        }
+        public void Move(int input)
+        {
+            if (board[input] != ' ')
+            {
+                Console.WriteLine("Position already occupied. Enter new position");
+            }
+            Console.WriteLine("What do you want to enter (X or O)");
+            char inputChar = char.Parse(Console.ReadLine());
+            board[input] = inputChar;
+        }
+
+    }
+}
