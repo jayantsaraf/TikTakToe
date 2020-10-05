63fbc8a4cbb961d4162117865d456ce98a54b160
jayantsaraf <sarafjayant24@gmail.com>
2020-10-05 14:04:07 +0530

    [jayant]Add. Created board

---
+++ TikTakToe/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TikTakToe
+{
+    public class TikTakToeGame
+    {
+        public void UC1_CreateBoard()
+        {
+            int[] board = new int[20];
+        }
+    }
+    public class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Tik Tak Toe program!");
+            TikTakToeGame game = new TikTakToeGame();
+            game.UC1_CreateBoard();
+        }
+    }
+}
