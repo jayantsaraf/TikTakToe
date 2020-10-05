45c252a41eabf507c5e40cb565f4e773d63075b5
jayantsaraf <sarafjayant24@gmail.com>
2020-10-05 14:23:34 +0530

    [jayant]Refactor. Updated Create Board function

--- TikTakToe/Program.cs
+++ TikTakToe/Program.cs
@@ -2,27 +2,13 @@
 
 namespace TikTakToe
 {
-    public class TikTakToeGame
-    {
-        public void UC1_CreateBoard()
-        {
-            int i = 1;
-            int[] board = new int[10];
-            for(i=1;i<=10;i++)
-            {
-                board[i++] = 0;
-            }
-            
-        }
-    }
     public class Program
-    {
-        
+    {      
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Tik Tak Toe program!");
             TikTakToeGame game = new TikTakToeGame();
-            game.UC1_CreateBoard();
+            game.CreateBoard();
         }
     }
 }
---
+++ TikTakToe/TikTakToeGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TikTakToe
+{
+    public class TikTakToeGame
+    {
+        public void CreateBoard()
+        {
+            int i = 0;
+            char[] board = new char[10];
+            for (i = 1; i < 10; i++)
+            {
+                board[i] = ' ';
+            }
+        }
+    }
+}
